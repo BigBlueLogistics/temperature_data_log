585143eacbd7857c4d182bd7026515e32a4f221e
Roelan Geil <roelaneroy@gmail.com>
2023-05-09 08:47:11 +0800

    refactor: implement server-side data fetching in Temperature component

--- src/pages/sample/index.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const Stream = dynamic(() => import("../../components/Stream"), { ssr: false });
-
-function Sample() {
-  return <Stream />;
-}
-
-export default Sample;
--- src/pages/temperature/index.tsx
+++ src/pages/temperature/index.tsx
@@ -1,84 +1,25 @@
-import { Inter } from "next/font/google";
 import dynamic from "next/dynamic";
-import data from "./data";
+import { NextPage } from "next";
+import { getTemperature } from "@/services/room";
 
-const inter = Inter({ subsets: ["latin"] });
+const Stream = dynamic(() => import("../../components/Stream"), { ssr: false });
 
-const Gauge = dynamic(() => import("../../components/Gauge"), {
-  ssr: false,
-});
+type Props = {
+  room: any;
+};
 
-function Temperature() {
-  const tempData = data.temp || null;
+const Temperature: NextPage<Props> = ({ room }) => {
+  const initialRoomData = JSON.parse(room) || {};
+  return <Stream initialTemp={initialRoomData} />;
+};
 
-  return (
-    <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 md:gap-y-4 md:gap-x-2 lg:text-left">
-      {tempData
-        ? tempData.map((item) => (
-            <div
-              key={item.id}
-              className="group rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30"
-            >
-              <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-                {item.title} {item.id}
-              </h2>
-              <Gauge
-                renderTo={`${item.title} ${item.id}`}
-                width={200}
-                height={200}
-                units={"°C"}
-                title={"Temperature"}
-                value={item.value}
-                minValue={0}
-                maxValue={40}
-                majorTicks={[40, 35, 30, 25, 20, 15, 10, 5, 0]}
-                minorTicks={5}
-                strokeTicks={true}
-                barStartPosition="right"
-                highlights={[
-                  {
-                    from: 0,
-                    to: 10,
-                    color: "red",
-                  },
-                  {
-                    from: 10,
-                    to: 15,
-                    color: "yellow",
-                  },
-                  {
-                    from: 15,
-                    to: 25,
-                    color: "#04fe2d",
-                  },
-                  {
-                    from: 25,
-                    to: 30,
-                    color: "yellow",
-                  },
-                  {
-                    from: 30,
-                    to: 40,
-                    color: "red",
-                  },
-                ]}
-                valueBox
-                colorPlate={"#fff"}
-                borderShadowWidth={0}
-                borders={false}
-                needleType={"arrow"}
-                needleWidth={2}
-                needleCircleSize={7}
-                needleCircleOuter
-                needleCircleInner={false}
-                animationDuration={10000}
-                animationRule={"linear"}
-              />
-            </div>
-          ))
-        : null}
-    </div>
-  );
+export async function getServerSideProps() {
+  const room = await getTemperature();
+  return {
+    props: {
+      room: JSON.stringify(room),
+    },
+  };
 }
 
 export default Temperature;
