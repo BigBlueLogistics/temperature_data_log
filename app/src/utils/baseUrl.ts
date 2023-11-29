349f1e474cf267bb7c0d79d3d6ffb086d15703f0
Roelan <roelaneroy@gmail.com>
2023-11-29 11:10:52 +0800

    remove environment variables from .env file

--- app/src/utils/baseUrl.ts
+++ app/src/utils/baseUrl.ts
@@ -1,8 +1,7 @@
 function baseUrl() {
-  const protocol =
-    process.env.APP_PROTOCOL || process.env.NEXT_PUBLIC_APP_PROTOCOL;
-  const domain = process.env.APP_DOMAIN || process.env.NEXT_PUBLIC_APP_DOMAIN;
-  const port = process.env.PORT || process.env.NEXT_PUBLIC_APP_PORT;
+  const protocol = process.env.NEXT_PUBLIC_APP_PROTOCOL;
+  const domain = process.env.NEXT_PUBLIC_APP_DOMAIN;
+  const port = process.env.NEXT_PUBLIC_APP_PORT;
 
   return `${protocol}://${domain}:${port}`;
 }
