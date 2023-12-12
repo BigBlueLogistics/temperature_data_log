ca9e6e7611c08b3f9f00fb6342a501c7fca0beb6
Roelan <roelaneroy@gmail.com>
2023-12-12 16:27:34 +0800

    Revert "Revert "Revert "remove environment variables from .env file"""
    This reverts commit 1b765ed186c69a36542bbb5c68b2d25a8a1f2b20.

--- app/src/utils/baseUrl.ts
+++ app/src/utils/baseUrl.ts
@@ -1,7 +1,8 @@
 function baseUrl() {
-  const protocol = process.env.NEXT_PUBLIC_APP_PROTOCOL;
-  const domain = process.env.NEXT_PUBLIC_APP_DOMAIN;
-  const port = process.env.NEXT_PUBLIC_APP_PORT;
+  const protocol =
+    process.env.APP_PROTOCOL || process.env.NEXT_PUBLIC_APP_PROTOCOL;
+  const domain = process.env.APP_DOMAIN || process.env.NEXT_PUBLIC_APP_DOMAIN;
+  const port = process.env.PORT || process.env.NEXT_PUBLIC_APP_PORT;
 
   return `${protocol}://${domain}:${port}`;
 }
