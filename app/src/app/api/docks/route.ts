702570654324e0d311a3e51c95e77f5916ce9f74
Roelan <roelaneroy@gmail.com>
2023-12-12 14:42:24 +0800

    Revert "add 3-5 monitor to API"
    This reverts commit 852f65c6fb3eb67aa092bc5f183ade636e6bd728.

--- app/src/app/api/docks/route.ts
+++ app/src/app/api/docks/route.ts
@@ -4,7 +4,7 @@ import { ObjectId } from "mongodb";
 import { getErrorMessage } from "@/utils";
 
 export async function POST(req: NextRequest) {
-  const updateOne = (dockId: string, dockStatus: "VACANT" | "OCCUPIED") => {
+  const updateOne = (dockId: string, dockStatus: "vacant" | "occupied") => {
     return {
       updateOne: {
         filter: { _id: new ObjectId(dockId), status: { $ne: dockStatus } },
@@ -14,14 +14,14 @@ export async function POST(req: NextRequest) {
               status: dockStatus,
               time_in: {
                 $cond: {
-                  if: { $eq: [dockStatus, "OCCUPIED"] },
+                  if: { $eq: [dockStatus, "occupied"] },
                   then: new Date(),
                   else: "$time_in",
                 },
               },
               time_out: {
                 $cond: {
-                  if: { $eq: [dockStatus, "VACANT"] },
+                  if: { $eq: [dockStatus, "vacant"] },
                   then: new Date(),
                   else: null,
                 },
@@ -53,13 +53,7 @@ export async function POST(req: NextRequest) {
 
     const post = await db
       .collection("docks")
-      .bulkWrite([
-        updateOne(dock_1, sensor_1),
-        updateOne(dock_2, sensor_2),
-        updateOne(dock_3, sensor_3),
-        updateOne(dock_4, sensor_4),
-        updateOne(dock_5, sensor_5),
-      ]);
+      .bulkWrite([updateOne(dock_1, sensor_1), updateOne(dock_2, sensor_2)]);
 
     return NextResponse.json({ status: "succeeded", data: post });
   } catch (e) {
